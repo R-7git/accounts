d311af1887be1cb826007bca83a3da5c15295f63
Roshan Singh Hari Rajput <110955286+Rosh121093@users.noreply.github.com>
2024-12-22 15:56:27 +0530

    Initial commit

---
+++ src/test/java/com/eazybytes/accounts/AccountsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eazybytes.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
